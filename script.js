443e9fb5804e4a825504302807b7946b9d87876f
DeekshithVanka <vankadeekshith333@gmail.com>
2024-12-22 10:21:59 +0530

    second day of namaste react

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const ele = React.createElement('h1',{id:98,name:'sai'},'this is my first react element')
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+
+const ele = React.createElement('h1',{id:98,name:'sai'},React.createElement('h2',{id:98,name:'sai'},'hello i am h2'))
 console.log(ele)
 const r=document.getElementById("root")
 const ro=ReactDOM.createRoot(r)
