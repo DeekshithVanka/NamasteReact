0324f866c701ccfe716a44bf314befbb9246c30f
DeekshithVanka <vankadeekshith333@gmail.com>
2024-12-26 22:36:26 +0530

    Episode 4

--- script.js
+++ script.js
@@ -1,25 +1,98 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
-const  eleZero= React.createElement('h1',{id:98,name:'sai'},React.createElement('h2',{id:98,name:'sai'},'hello i am h2'))
-console.log(eleZero)
+const Header=()=>{
 
+    return (
+        < div className="header">
+           <div className="logo-container">
+           {/* <img  src="./cart.png" alt="Nothing is rendered"/> */}
+             <h1>
+                Mobio
+             </h1>
+           </div>
+           <div className="nav-items">
+            <ul>
+        <li> Home </li>
+        <li> About us </li>
+        <li> contact us </li>
+        <li> cart  </li>1
+            </ul>
+            </div>
+       
+        </div>
+    )
 
-
-const pi=5000
-
-const Title=()=><h2>Ru </h2>
-const Header=()=> (<h1> 
-   
-    <Title/>
-    inside Header</h1>)
-
+}
+const Card=(props)=>{
     
-var eleOne= (<h1 className="One" id="one" style={{color:"red",textAlign:"center"}}>This is my{<Header/>} 
-    first jsx tag </h1>)
+    const {name,age}=props
+    
 
 
-const r=document.getElementById("root")
-const ro=ReactDOM.createRoot(r)
-ro.render(ele1)
+    return (
+        <div className="rescard">
+
+            
+            <h2>   {name}</h2>
+            <h2>   {age}</h2>
+            <h2>   Price</h2>
+            <h2>   rating</h2>
+            
+            
+            
+            
+              </div>
+    )
+}
+
+const Body=()=>{
+
+    const resobj=[{
+        id:1,
+        name:"sai",
+        age:20
+    },{
+        id:2,
+        name:"shetty",
+        age:18
+    },{
+        id:3,
+        name:"kiran",
+        age:25
+    }]
+
+   return (<div className="body" >
+    
+    <div className="res-container" >
+{
+    resobj.map((restrent)=>(
+        <Card  key={restrent.id} name={restrent.name} age={restrent.age}/>
+    )
+
+    )
+}
+ </div>
+ </div>)
+}
+
+
+const Footer=()=>{
+}
+const Applayout=()=>{
+    return (
+        <div className="app">
+        <Header/>
+
+
+        <Body/>
+        <Footer/>
+        </div>
+
+    )
+}
+
+const rootelement=document.getElementById("root")
+const root=ReactDOM.createRoot(rootelement)
+root.render(<Applayout/>)
